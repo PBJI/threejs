2f2a3c6eb8f0859e2b7c21f3a2e3b5286c9fe7e4
Purshotam <purshotam@zeazonz.com>
2025-07-19 15:12:54 +0530

    v2.1.2 Fixed import paths with .js extension instead of just file names

--- src/core/controls.js
+++ src/core/controls.js
@@ -1,4 +1,4 @@
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { OrbitControls } from 'https://esm.run/three@0.160.0/examples/jsm/controls/OrbitControls.js'
 import { camera } from './camera.js'
 import { renderer } from './renderer.js'
 export const controls = new OrbitControls(camera, renderer.domElement)
